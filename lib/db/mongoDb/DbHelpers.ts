a2a37054e58e14f150c31d9aba3073f56ad5c933
armanmac <arman.piloyan@gmail.com>
2019-11-13 21:50:06 +0400

    more cleanup

--- lib/db/mongoDb/DbHelpers.ts
+++ lib/db/mongoDb/DbHelpers.ts
@@ -3,10 +3,10 @@ import position from "./models/Position";
 export class DbHelpers {
 
 
-    async createPositionRecord(id: string, lastPositon: string, score: number) {
+    async createPositionRecord(id: string, lastPosition: string, score: number) {
         return await position.create({
             procedureId: id,
-            lastPosition: lastPositon,
+            lastPosition: lastPosition,
             currentScore: score
 
         });
